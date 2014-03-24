8445447fd67d3beca3d40f6010ce57a7314d8c04
Gautier Hayoun <ghayoun@gmail.com>
2014-03-24 18:47:01 +0000

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-xstatic',
-    version='0.0.1',
+    version='0.0.2',
     description='Django helpers to use XStatic packages in Django projects',
     classifiers=[
         'License :: OSI Approved :: MIT License',
